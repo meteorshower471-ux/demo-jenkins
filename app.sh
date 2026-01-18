3db38052f12fb28fd7dd6d748663732f448eecbf
meteorshower471-ux <meteorshower471@gmail.com>
2026-01-18 17:02:01 +0530

    dg

--- app.sh
+++ app.sh
@@ -5,7 +5,8 @@ mkdir -p output
 cat <<EOF > output/index.html
 <html>
   <body>
-    <h1>Jenkins Pipeline Ran Successfully</h1>
+    <h1>Jenkins + Docker running successfully</h1>
+    <p>Built on $(date)</p>
   </body>
 </html>
 EOF
